8b04bbbeec6f9e35beba2ffafa7d9bbf8b3423d7
cricklet <rilee@mapsaurus.com>
2013-02-25 14:50:14 -0500

    The example is now super simple. Make sure you look at ExampleFragment in addition to ExampleActivity

---
+++ example/src/com/mapsaurus/panesexample/ExampleActivity.java
@@ -0,0 +1,74 @@
+package com.mapsaurus.panesexample;
+
+import com.mapsaurus.paneslayout.PanesActivity;
+import com.mapsaurus.paneslayout.PanesSizer.PaneSizer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class ExampleActivity extends PanesActivity {
+
+	/**
+	 * The PaneSizer allows you to programatically size each pane.
+	 * 
+	 * Type is an arbitrary integer that represents the type of view or fragment 
+	 * contained within a pane.
+	 * 
+	 * In this example, there are only two types, 'default' and 'unknown.'
+	 * Panes associated with ExampleFragments are given type 'default.'
+	 * 
+	 * Certain panes can also be focused. This means that swipe gestures will
+	 * not be detected on those panes.
+	 */
+	private class ExamplePaneSizer implements PaneSizer {
+		private static final int DEFAULT_PANE_TYPE = 0;
+		private static final int UNKNOWN_PANE_TYPE = -1;
+
+		@Override
+		public int getWidth(int index, int type, int parentWidth, int parentHeight) {
+			if (parentWidth > parentHeight) {
+				if (type == DEFAULT_PANE_TYPE)
+					return (int) (0.4 * parentWidth);
+				else throw new IllegalStateException("Pane has unknown type");
+			} else {
+				if (type == DEFAULT_PANE_TYPE)
+					return (int) (0.6 * parentWidth);
+				else throw new IllegalStateException("Pane has unknown type");
+			}
+		}
+
+		@Override
+		public int getType(Object o) {
+			if (o instanceof ExampleFragment)
+				return DEFAULT_PANE_TYPE;
+			else return UNKNOWN_PANE_TYPE;
+		}
+
+		@Override
+		public boolean getFocused(Object o) {
+			return false;
+		}
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setPaneSizer(new ExamplePaneSizer());
+
+		// Lets setup a menu and a first pane!
+		if (savedInstanceState == null) {
+			Fragment menu = new ExampleFragment();
+			Fragment first = new ExampleFragment();
+			setMenuFragment(menu);
+			addFragment(menu, first);
+			
+			// if you wanted to add more fragments after these ones, you can do:
+			// Fragment second = new ExampleFragment();
+			// Fragment third = new ExampleFragment();
+			// addFragment(first, second);
+			// addFragment(second, third);
+		}
+	}
+
+}
---
+++ example/src/com/mapsaurus/panesexample/ExampleFragment.java
@@ -0,0 +1,174 @@
+package com.mapsaurus.panesexample;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
+import com.mapsaurus.panesexample.R;
+import com.mapsaurus.paneslayout.FragmentLauncher;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class ExampleFragment extends Fragment {
+
+	private static int sExampleNum = 0;
+
+	private TextView text1;
+	private TextView text2;
+	private Button addButton;
+	private View parentView;
+
+	private int text1Val = -1;
+	private int text2Val = -1;
+
+	/**
+	 * Create a new ExampleFragment and add it!
+	 */
+	private void addExampleFragment() {
+		// create a new fragment
+		Fragment f = new ExampleFragment();
+		
+		// get the activity and add the new fragment after this one!
+		Activity a = getActivity();
+		if (a != null && a instanceof FragmentLauncher)
+			((FragmentLauncher) a).addFragment(ExampleFragment.this, f);
+	}
+
+	public ExampleFragment() {
+		super();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		setRetainInstance(true);
+
+		parentView = inflater.inflate(R.layout.example_fragment, container, false);
+		if (color != -1) {
+			parentView.setBackgroundColor(color);
+		} else {
+			randomizeColor();
+		}
+
+		parentView.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				randomizeColor();
+			}
+		});
+
+		addButton = (Button) parentView.findViewById(R.id.add);
+		addButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				addExampleFragment();
+			}
+		});
+
+		if (text1Val == -1)
+			text1Val = sExampleNum ++;
+
+		text1 = (TextView) parentView.findViewById(R.id.text1);
+		text1.setText("" + text1Val);
+
+		text2 = (TextView) parentView.findViewById(R.id.text2);
+		text2.setText("...");
+
+		return parentView;
+	}
+
+
+	/* *********************************************************************
+	 * Deal with coloring the fragment and showing the amount of time
+	 * it's been alive.
+	 * ********************************************************************* */
+
+	/**
+	 * Randomize the background of this fragment
+	 */
+	public void randomizeColor() {
+		color = Color.rgb((int) (Math.random() * 255),
+				(int) (Math.random() * 255), (int) (Math.random() * 255));
+		parentView.setBackgroundColor(color);
+	}
+
+	/**
+	 * Current color of this fragment
+	 */
+	private int color = -1;
+
+	/**
+	 * These variables control the threads
+	 */
+	private boolean paused = true, destroyed = false;
+
+	private Thread thread = new Thread() {
+		private double fps = 1;
+
+		public void run() {
+			while (!destroyed) {
+				if (paused) {synchronized (this) {
+					try {
+						wait();
+					} catch (InterruptedException e) {
+					}
+				}}
+
+				long startTime = System.currentTimeMillis();
+
+				text2Val ++;
+				text2.post(new Runnable() {public void run() {
+					text2.setText("" + text2Val);
+				}});
+
+				long endTime = System.currentTimeMillis();
+				long dTime = endTime - startTime;
+				try {
+					Thread.sleep((long) Math.max(1000 / fps - dTime, 0));
+				} catch (InterruptedException e) {
+				}
+			}
+		}
+	};
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		if (!thread.isAlive())
+			thread.start();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		paused = false;
+		synchronized (thread) {
+			thread.notify();
+		}
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		paused = true;
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		destroyed = true;
+	}
+
+}
